7cbe8dd3d0ab645190c6052465ac209d2e5dc943
jaydendavis746-debug <jaydendavis746@gmail.com>
2026-09-07 12:29:55 +0100

    Ed sheeran page

--- src/App.jsx
+++ src/App.jsx
@@ -1,121 +1,77 @@
 import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
+  const [like, setLike]= useState(false)
+  const [btnText, setBtnText] = useState('click to like !')
+  const [InputText, setInputText] = useState('')
+  
+
+  const handleText = (e)=>{
+    setInputText(e.target.value)
+  }
+
+  const handleSubmit = (e)=>{
+    e.preventDefault()
+
+    const songList = document.getElementById('songList')
+    const song = document.createElement('li')
+
+    song.textContent = InputText
+    songList.appendChild(song)
+
+    setInputText('')
+  }
+
+
+  const updateLike = ()=>{
+    setLike(!like)    
+   like ? setBtnText('Unliked') : setBtnText('Liked')
+  }
+
+  return(
+
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    <h1>Ed Sheeran </h1>
+    <h2>Pop Artist</h2>
+    <p>
+      Edward Christopher Sheeran is an English singer-songwriter. 
+      Born in Halifax, West Yorkshire, and raised in Framlingham, Suffolk, 
+      he began writing songs around the age of eleven. In early 2011, 
+      Sheeran independently released the extended play No.5 Collaborations Project. 
+      He signed with Asylum Records the same year.
+    </p>
+    <div className='albums'>
+      <h2>Latest Album: Divide</h2>
+      <button className='LikeBtn' onClick={updateLike}>{btnText}</button>
+      <div className='AlbumCover'>
+        <img src="../src/assets/divide.png" 
+        alt="Divide cover art" />
+      </div>
+      <h3>Song List</h3>
+      <ul id='songList'>
+        <li>Eraser</li>
+        <li>Catsle on the hill</li>
+        <li>Dive</li>
+        <li>Shape of you</li>
+        <li>Perfect</li>
+        <li>Galway Girl</li>
+        <li>Happier</li>
+        <li>New Man</li>
+        <li>Hearts Don't Break Around Here</li>
+        <li>What Do I Know</li>
+        <li>How Would You Feel</li>
+        <li>Supermarket Flowers </li>
+      </ul>
+    </div>
 
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+    <form onSubmit={handleSubmit}>
+      <input type="text" placeholder='Add a song' value={InputText} onChange={handleText} required/>
+      <button>Add</button>
+    </form>
     </>
+
+
   )
 }
 
