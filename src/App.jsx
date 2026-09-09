6e589cb87b49d78f14a1aaaf1f99dc6d73c63830
JGiles03 <josephsgiles@gmail.com>
2026-09-09 16:32:24 +0100

    dont merge

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,18 @@ function App() {
   const [btnText, setBtnText] = useState('click to like !')
   const [InputText, setInputText] = useState('')
   const [songs, setSongs] = useState([{name: "Eraser", liked: false},
-    {name: "Castle on the hill", liked: false},
-    {name: "Dive", liked: false},
-    {name: "Shape of you", liked: false},
-    {name: "Perfect", liked: false},
-    {name: "Galway Girl", liked: true},
-    {name: "Happier", liked: false},
-    {name: "New Man", liked: false},
-    {name: "Hearts Don't Break Around Here", liked: false},
-    {name: "What Do I Know", liked: false},
-    {name: "How Would You Feel", liked: false},
-    {name: "Supermarket Flowers", liked: false}
-  ])
+    {name: "Castle on the hill", liked: false},
+    {name: "Dive", liked: false},
+    {name: "Shape of you", liked: false},
+    {name: "Perfect", liked: false},
+    {name: "Galway Girl", liked: true},
+    {name: "Happier", liked: false},
+    {name: "New Man", liked: false},
+    {name: "Hearts Don't Break Around Here", liked: false},
+    {name: "What Do I Know", liked: false},
+    {name: "How Would You Feel", liked: false},
+    {name: "Supermarket Flowers", liked: false}
+  ])
 
   const handleText = (e)=>{
     setInputText(e.target.value)
